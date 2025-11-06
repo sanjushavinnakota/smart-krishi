04378439b693d696bee62ab2b2e8ea21491e1f83
sanjushavinnakota <sanjushavinnakota@gmail.com>
2025-11-07 00:36:40 +0530

    "auth"

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/authcontroller"); 
+const { registerUser, loginUser } = require("../controllers/authController.js "); 
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
